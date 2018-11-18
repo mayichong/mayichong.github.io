d3a785fb795c2b32f7d9851179cb40506eba3273
Yichong Ma <mayichong123@gmail.com>
2018-11-18 03:27:46 -0500

    startService

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var believe = "BELIEVE";
-    var believebold = believe.bold();
-    var colorbold = believebold.fontcolor("red");
-    var phrase = "I never " + colorbold + " in luck without effort, so I create luck by myself, day by day.";
-    document.getElementById("displayinfo").innerHTML =phrase;
-}
-
